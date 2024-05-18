c5e30d392d9439ae8f3e25f2f9bbf3bf74a74f43
Abdul Sami <aitoolslg@gmail.com>
2024-05-18 20:58:39 +0500

    functions,callback ,truthy and falsy values etc

---
+++ practice_02.js
@@ -0,0 +1,45 @@
+/*let age=20;
+if([]){
+    console.log("Age is true");
+}else
+{
+    console.log("Age is false");
+}
+
+text=(age<9) ? console.log('True'):console.log('False')
+
+function move(direction, steps){
+    console.log(`Moving ${steps} steps to the ${direction}`);
+}
+move('East', 9);
+
+const sum=(a,b)=>{
+
+    return a+b;
+}
+
+const sum=(a,b)=>a+b;
+console.log(sum(3,5))
+console.log( typeof(NaN))
+
+// create a example of call back
+// call back
+
+var talk = function(sayHi){
+    sayHi();
+}
+var sayHi = function(){
+    console.log('Hi');
+}
+//sayHi();
+talk(sayHi)
+
+function greeting(name){
+    console.log('Hello '+name);
+}
+function processUserInput(callback){
+    let name='John';
+    callback(name);
+}
+processUserInput(greeting);
+*/
